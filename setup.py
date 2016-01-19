e7d2521ea8a6c3676bf953c2c60f0305b4cdddd8
Lajos Santa <santa@deverto.com>
2016-01-19 14:08:22 +0100

    Add command for register external tools manually

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = "asd",
     description = "Various command line helper scripts`",
-    version = "1.1.1",
+    version = "1.2.0",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
     url = 'https://github.com/voidpp/asd.git',
