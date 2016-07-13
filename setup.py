d8eb23d6f467e53ebf1b4d28f1a397ff881e3eeb
Lajos Santa <santa.lajos@coldline.hu>
2016-07-13 22:01:08 +0200

    Add tool to generate openvpn client configs

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name = "asd",
     description = "Various command line helper scripts`",
-    version = "1.2.0",
+    version = "1.3.0",
     author = 'Lajos Santa',
     author_email = 'santa.lajos@coldline.hu',
     url = 'https://github.com/voidpp/asd.git',
     install_requires = [
         "argcomplete==1.0.0",
-        "voidpp-tools>=1.5.4",
+        "voidpp-tools==1.8.1",
     ],
     scripts = [
         'bin/asd',
